05b18e8babab2f6c07cf5c2465a249ea10f037ac
xingke123 <763772093@qq.com>
2018-03-03 11:00:52 +0800

    Singleton

--- Singleton/ehan_singledon.cpp
+++ Singleton/ehan_singledon.cpp
@@ -1,4 +1,13 @@
+//饿汉单例模式，在类初始化时便自行创建了实例
 class Singleton{
 	private:
-	
+	    Singleton(){};
+	public:
+		static Singleton* GetInstance();
+	private:
+		static Singleton* instance_=new Singleton();
+}
+
+Singleton* Singleton::GetInstance(){
+	return instance_;
 }
--- Singleton/lanhan_singleton.cpp
+++ Singleton/lanhan_singleton.cpp
@@ -1,3 +1,26 @@
 //懒汉单例模式，调用GetIstance时才创建实例
 
 #include<pthread.h>
+
+class Singleton{
+private:
+	Singleton(){}
+public:
+	static Singleton* GetInstance();
+private:
+	static Singleton* instance_;
+	static pthread_mutex_t mutex_t;
+}
+
+Singleton* Singleton::instance_=NULL;
+pthread_mutex_t Singleton::mutex_t;
+
+Singleton* Singleton::GetInstance(){
+	if(instance_==NULL){
+		pthread_mutex_lock(&mutex_t);
+		if(instance_==NULL)
+			instance_=new Singleton();
+		pthread_mutex_unlock(&mutex_t);
+	}
+	return instance_;
+}
